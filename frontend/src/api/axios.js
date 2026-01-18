5334ed9db9d492b7b1497bbf300d343da2ba0367
roshanrl10 <itsmine580@gmail.com>
2026-01-19 00:19:36 +0545

    error correction

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Adjust if port is different
+    baseURL: 'http://localhost:5001/api', // Adjust if port is different
     withCredentials: true // Important for maintaining sessions
 });
 
